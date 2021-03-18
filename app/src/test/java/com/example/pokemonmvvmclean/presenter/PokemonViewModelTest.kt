bdcbcb9d20836edac9b97bea0ab0f0c2dfbcf10f
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-03-18 17:03:29 -0300

    Teste unitário finalizado, retornando com sucesso o esperado por cada camada da aplicação.

---
+++ app/src/test/java/com/example/pokemonmvvmclean/presenter/PokemonViewModelTest.kt
@@ -0,0 +1,33 @@
+package com.example.pokemonmvvmclean.presenter
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.pokemonmvvmclean.domain.GetPokemonsUseCase
+import com.example.pokemonmvvmclean.domain.model.PokemonFactory
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class PokemonViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val useCase = mockk<GetPokemonsUseCase>()
+    private val viewModel = PokemonViewModel(useCase)
+
+    @Test
+    fun `when useCase is instantiated with success return a list`() = runBlocking {
+        // GIVEN
+        coEvery { useCase.invoke() } returns PokemonFactory.pokemons
+
+        //WHEN
+        val result = useCase()
+
+        //THEN
+        Assert.assertEquals(result.size, PokemonFactory.pokemons.size)
+    }
+}
---
+++ app/src/test/java/com/example/pokemonmvvmclean/presenter/model/PokemonUiModelFactory.kt
@@ -0,0 +1,23 @@
+package com.example.pokemonmvvmclean.presenter.model
+
+object PokemonUiModelFactory {
+
+    val pokemons = listOf(
+        PokemonUiModel(
+            "Bulbasaur",
+            "http://www.serebii.net/pokemongo/pokemon/001.png"
+        ),
+        PokemonUiModel(
+            "Ivysaur",
+            "http://www.serebii.net/pokemongo/pokemon/002.png"
+        ),
+        PokemonUiModel(
+            "Venusaur",
+            "http://www.serebii.net/pokemongo/pokemon/003.png"
+        ),
+        PokemonUiModel(
+            "Charmander",
+            "http://www.serebii.net/pokemongo/pokemon/004.png"
+        )
+    )
+}
